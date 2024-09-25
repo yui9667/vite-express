e1f5d7dedc7fa27c1f4fb91df8cc459ed2952425
Yui Jensen <kyanyui67@gmail.com>
2024-09-25 15:15:25 +0200

    added object aand styled css

--- server/server.js
+++ server/server.js
@@ -4,10 +4,22 @@ const cors = require('cors');
 const corsOptions = {
   origin: ['http://localhost:5173'],
 };
+
+const info = {
+  name: 'Yui',
+  age: '28',
+  hobby: 'Dance and play Drums',
+};
+
+const message = 'Nice to meet you!';
+
 app.use(cors(corsOptions));
+//first route
 app.get('/api', (req, res) => {
-  res.json({ cars: ['Mercedes', 'Toyota', ' Ford', 'Opel', 'Volvo'] });
+  res.json(info);
 });
+//Second route
+
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
